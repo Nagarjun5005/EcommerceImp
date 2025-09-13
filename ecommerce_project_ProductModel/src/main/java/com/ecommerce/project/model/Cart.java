83c0aa4a48a2b0071e25af616c6426e969af1ff0
Nagarjun <nagarjun20n@gmail.com>
2025-09-13 19:00:45 +0530

    implement cart and cartItems module

---
+++ ecommerce_project_ProductModel/src/main/java/com/ecommerce/project/model/Cart.java
@@ -0,0 +1,33 @@
+package com.ecommerce.project.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cartId;
+
+    private Double totalPrice=0.0;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User userId;
+
+    @OneToMany(mappedBy = "cart",cascade = {
+            CascadeType.PERSIST,CascadeType.REMOVE,CascadeType.MERGE
+    })
+    private List<CartItem> cartItems=new ArrayList<>();
+}
